877405be653c82738d17beccd0d81107e708e602
nikoalfin <nikoalfinsetyawan@gmail.com>
2024-08-17 07:26:48 +0700

    resolve DetailNewsError

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
 import {createBrowserRouter,RouterProvider} from 'react-router-dom';
-import App from './App.jsx'
 import './index.css';
 import { FilterProvider } from './Context/FilterContext.jsx';
 import HomePage from './Pages/HomePage.jsx';
